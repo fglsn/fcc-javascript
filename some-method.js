7618834e8ca854179eaad879ded79930b1a5cd06
Ilona Shakurova <ishakuro@student.hive.fi>
2022-05-30 16:37:52 +0300

    Add freecodcamp functional programming

---
+++ some-method.js
@@ -0,0 +1,8 @@
+//Use the some method inside the checkPositive function to check if any 
+//element in arr is positive. 
+//The function should return a Boolean value.
+
+function checkPositive(arr) {
+	return arr.some(elem => elem > 0);
+  }
+  checkPositive([1, 2, 3, -4, 5]);
