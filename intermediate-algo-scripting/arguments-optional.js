841febf4265afa3785f618fade8ee20ef7b5e43b
Ilona Shakurova <ishakuro@student.hive.fi>
2022-06-01 16:50:03 +0300

    Add arguments optional

---
+++ intermediate-algo-scripting/arguments-optional.js
@@ -0,0 +1,32 @@
+// Arguments Optional
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+// For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+// Calling this returned function with a single argument will then return the sum:
+//		var sumTwoAnd = addTogether(2);
+//		sumTwoAnd(3) returns 5.
+// If either argument isn't a valid number, return undefined.
+
+function addTogether() {
+	let x = arguments[0];
+	let y = arguments[1];
+	if (typeof(x) !== "number")
+	  return undefined;
+	if (!y)
+	  return (y) => addTogether(x, y);
+	if (typeof(y) !== "number")
+	  return undefined;
+	return (x + y);
+  }
+
+
+
+var sumTwoAnd = addTogether(2);
+console.log(sumTwoAnd(3));
+
+// addTogether(2, 3) should return 5.
+// addTogether(23, 30) should return 53.
+// addTogether(5)(7) should return 12.
+// addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ") should return undefined.
+// addTogether(2, "3") should return undefined.
+// addTogether(2)([3]) should return undefined.
+// addTogether("2", 3) should return undefined.
